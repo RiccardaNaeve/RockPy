c1ea74fa53e5a237ad951c5ceac33306b566cf88
Michael Wack <MichaelWack@gmx.net>
2014-11-04 15:59:24 +0100

    _find_duplicate_variables implemented
    bugs with multiple variable rows

--- Structure/test_rockPyData.py
+++ Structure/test_rockPyData.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
-
+import numpy as np
 from RockPy.Structure.data import RockPyData
+
 
 __author__ = 'wack'
 
@@ -9,17 +10,28 @@ class TestRockPyData(TestCase):
     def setUp(self):
         # run before each test
         self.testdata = ((1, 2, 3, 4),
-                        (5, 6, 7, 8),
-                        (9, 10, 11, 12))
+                         (1, 6, 7, 8),
+                         (1, 2, 11, 12),
+                         (1, 6, 55, 66))
 
         self.col_names = ('F', 'Mx', 'My', 'Mz')
-        self.row_names = ('1.Zeile','2.Zeile','3.Zeile')
+        self.row_names = ('1.Zeile', '2.Zeile', '3.Zeile', '4.Zeile')
         self.units = ('T', 'mT', 'fT', 'pT')
 
-        self.RPD = RockPyData(column_names=self.col_names, row_names=self.row_names, units=self.units, data=self.testdata)
+        self.RPD = RockPyData(column_names=self.col_names, row_names=self.row_names, units=self.units,
+                              data=self.testdata)
 
     def test_column_names(self):
         self.assertEqual(self.RPD.column_names, list(self.col_names))
 
     def test_column_count(self):
-        self.assertEqual(self.RPD.column_count, len( self.col_names))
+        self.assertEqual(self.RPD.column_count, len(self.col_names))
+
+    def test__find_duplicate_variables(self):
+        #self.assertTrue((self.RPD._find_duplicate_variables()[0] == np.array([0, 1, 2])).all())
+        self.assertEqual(self.RPD._find_duplicate_variables(), [(0, 1, 2)])
+
+        # redefine variabe alias to the first two columns
+        self.RPD.define_alias('variable', ('F', 'Mx'))
+        print self.RPD._find_duplicate_variables()
+        self.assertEqual(self.RPD._find_duplicate_variables(), [(0, 2)])

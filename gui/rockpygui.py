3a6b372edbb1d901b6c49833bde47efd2197b0da
Michael Wack <MichaelWack@gmx.net>
2014-12-18 22:43:10 +0100

    identification of tree item onContext added

--- gui/rockpygui.py
+++ gui/rockpygui.py
@@ -148,12 +148,13 @@ class MainFrame(wx.Frame):
 
     def UpdateStudyNavTree(self):
         self.nav_tree.DeleteAllItems()  # clear the tree
-        if self.study == None:
-            # no study do nothing
-            return
 
         # Add a study as root node
         root = self.nav_tree.AddRoot("Study", ct_type=1)
+
+        if self.study == None:
+            # no study do nothing
+            return
 
         self.nav_tree.SetItemImage(root, 0, wx.TreeItemIcon_Normal)
         self.nav_tree.SetItemImage(root, 1, wx.TreeItemIcon_Expanded)
@@ -229,6 +230,11 @@ class MainFrame(wx.Frame):
         """
         Create and show dynamic context menu
         """
+
+        # identify tree item
+        hitobj, flags = self.nav_tree.HitTest(self.nav_tree.ScreenToClient(event.GetPosition()))
+        if isinstance( hitobj, ctc.GenericTreeItem):
+            print hitobj.GetText()
 
         # get some entries
         items = ('a', 'plots', 'delete')

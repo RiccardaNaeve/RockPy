e19833e0f22ecad9ca32d54829d268fafbc2d935
Michael Volk <grossstadtlegende@gmail.com>
2014-12-19 14:04:50 +0100

    sample, sample_group unjunked

--- VisualizeV2/Paleointensity.py
+++ VisualizeV2/Paleointensity.py
@@ -53,7 +53,7 @@ class Arai(base.Generic):
 
 
 class Multiple(base.Generic):
-    _required = {}
+    _required = {['thellier']}
     def initialize_visual(self):
         arai = Arai(plot_samples=self.study)
         dunlop = Dunlop(plot_samples=self.study)

36f8a5ca7b0320f31f2104e7515f0b9a842ffa4c
Michael Wack <MichaelWack@gmx.net>
2015-01-27 16:59:02 +0100

    minors

--- Tutorials/ani.py
+++ Tutorials/ani.py
@@ -21,24 +21,26 @@ def test():
 
     # add measurement, read from file
     M1 = sample1.add_measurement(mtype='anisotropy', mfile=ani_file2, machine='ani')
-
     M3 = sample2.add_simulation(mtype='anisotropy', evals=(1.5, 1.5, 0.3),
                                 mdirs=[[225.0, 0.0], [135.0, 0.0], [90.0, 45.0],
                                        [90.0, -45.0], [0.0, -45.0], [0.0, 45.0]])
+    #M4 = sample2.add_simulation(mtype='anisotropy', evals=(1.1, 1.5, 1.3),
+    #                            mdirs=[[225.0, 0.0], [135.0, 0.0], [90.0, 45.0],
+    #                                   [90.0, -45.0], [0.0, -45.0], [0.0, 45.0]])
 
     # print "M._data", M._data
     sg = RockPy.SampleGroup(sample_list=(sample1, sample2))
     #study = RockPy.Study(samplegroups=sg)
 
     M1.calculate_tensor()
-
     M3.calculate_tensor()
+    #M4.calculate_tensor()
 
-    #M.calc_all()  # broken
+    #M1.calc_all()  # broken
 
     print M1.results
-    #print M2.results
     print M3.results
+    #print M4.results
 
     plt = RockPy.Visualize.anisotropy.Anisotropy(sg, plt_primary='sample', plt_secondary=None)
     plt.show()

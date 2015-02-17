83476e767570f7724bb78dbcb7501c8a3f8f4535
Mike Volk <grossstadtlegende@gmail.com>
2015-02-17 16:08:50 +0100

    changed data

--- Measurements/test_hysteresis.py
+++ Measurements/test_hysteresis.py
@@ -1,12 +1,16 @@
+__author__ = 'mike'
 from unittest import TestCase
 import RockPy as rp
-
-__author__ = 'mike'
+import matplotlib.pyplot as plt
+import RockPy
+from os.path import join
 
 
 class TestHysteresis(TestCase):
     def setUp(self):
-        vsm_file = '../Tutorials/test_data/MUCVSM_test.hys'
+        # vsm_file = '../Tutorials/test_data/MUCVSM_test.hys'
+        vsm_file = join(RockPy.test_data_path, 'vsm', 'LTPY_527,1a_HYS_VSM#XX[mg]___#TEMP_300_K#STD000.000')
+
         self.VSM_test = rp.Sample('test')
         self.VSM_hys = self.VSM_test.add_measurement(mtype='hysteresis', mfile=vsm_file, machine='vsm')
 
@@ -18,3 +22,20 @@ class TestHysteresis(TestCase):
     def test_calculate_mrs(self):
         self.VSM_hys.calculate_mrs()
         print self.VSM_hys.results['mrs']
+
+
+    def test_grid_data(self):
+        # plt.plot(self.VSM_hys.get_grid())
+        self.VSM_hys.data_gridding(order='first')
+        self.VSM_hys.data_gridding(order='second')
+        plt.plot(self.VSM_hys.data['down_field']['field'].v, self.VSM_hys.data['down_field']['mag'].v, 'g.-')
+        plt.plot(self.VSM_hys.grid_data['down_field']['field'].v, self.VSM_hys.grid_data['down_field']['mag'].v, 'r.-')
+        plt.plot(self.VSM_hys.data['up_field']['field'].v, self.VSM_hys.data['up_field']['mag'].v, 'g.-')
+        plt.plot(self.VSM_hys.grid_data['up_field']['field'].v, self.VSM_hys.grid_data['up_field']['mag'].v, 'r.-')
+        # print self.VSM_hys.grid_data['down_field']
+        # print self.VSM_hys.data['down_field']
+        plt.show()
+
+
+    def test_correct_center(self):
+        self.VSM_hys.correct_center()

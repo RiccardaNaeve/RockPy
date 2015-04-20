19aa55eb92de96d0ad61c43c83cf977a75fcd43d
Mike Volk <grossstadtlegende@gmail.com>
2015-04-20 14:00:09 +0200

    Hysteresis.plotting removed from Visualize.Hysteresis -> in Visualize.base

--- Visualize/hysteresis.py
+++ Visualize/hysteresis.py
@@ -4,7 +4,8 @@ import Features.hysteresis
 
 
 class Hysteresis(base.Generic):
-    _required = ['hysteresis']
+    _required = ['hys']
+
 
     def initialize_visual(self):
         self.add_plot()
@@ -14,31 +15,11 @@ class Hysteresis(base.Generic):
         self.xlabel = 'Field'
         self.ylabel = 'Moment'
 
-    def plotting(self, samples, **plt_opt):
-        samples = self.get_plot_samples()
-        hys_dict = None
-
-        for sample in samples:
-            hys = sample.get_measurements(mtype='hysteresis')
-            hys_dict = sample.sort_mlist_in_ttype_dict(hys)
-
-            for ttype in hys_dict:
-                tvals = sorted(hys_dict[ttype].keys())
-                colors = self.create_heat_color_map(value_list=tvals, reverse=False)
-                for i, tval in enumerate(sorted(hys_dict[ttype].keys())):
-                    for h in hys_dict[ttype][tval]:
-                        for feature in self.standard_features:
-                            plt_opt = {'color':colors[i]}
-                            feature(h, **plt_opt)
-                            # print(feature, self.ax.get_xlim())
-
-        for feature in self.single_features:
-            feature()
-            # print(feature, self.ax.get_xlim())
-
     def feature_hys(self, hys_obj, **plt_opt):
+        print hys_obj
         hys_obj = hys_obj.normalize(**self.norm)
         lines, texts = Features.hysteresis.df_branch(self.ax, hys_obj, **plt_opt)
+        self._add_line_text_dict(lines, texts)
         lines, texts = Features.hysteresis.uf_branch(self.ax, hys_obj, **plt_opt)
         self._add_line_text_dict(lines, texts)
 

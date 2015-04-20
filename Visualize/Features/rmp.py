586cbb44735b88f61860b43a71eed54d2dfc9c8e
Mike Volk <grossstadtlegende@gmail.com>
2015-04-20 14:00:41 +0200

    rmp plot fix

--- Visualize/Features/rmp.py
+++ Visualize/Features/rmp.py
@@ -2,18 +2,30 @@ __author__ = 'mike'
 import numpy as np
 import RockPy.Functions.general
 
-def mom_temp(ax, rmp_obj, **plt):
+
+def mom_temp(ax, rmp_obj, **plt_opt):
     color = {'up_temp': 'r', 'down_temp': 'b'}
-    for seg in rmp_obj._data:
-        lines = ax.plot(rmp_obj.data[seg]['temp'].v, rmp_obj.data[seg]['mag'].v, '.-', color=color[seg])
+    marker = plt_opt.pop('marker', '')
+    ls = plt_opt.pop('ls', '-')
+
+    for seg in rmp_obj.data:
+        lines = ax.plot(rmp_obj.data[seg]['temp'].v, rmp_obj.data[seg]['mag'].v,
+                        marker=marker,
+                        ls=ls,
+                        color=color[seg])
     return lines
 
-def dmom_dtemp(ax, rmp_obj, **plt):
+
+def dmom_dtemp(ax, rmp_obj, **plt_opt):
     color = {'up_temp': 'r', 'down_temp': 'b'}
-    ymax = max(ax.get_ylim())
+    marker = plt_opt.pop('marker', '')
+    ls = plt_opt.pop('ls', '-')
+    alpha = plt_opt.pop('alpha', 0.5)
+
     for seg in rmp_obj._data:
-        print(seg)
-        segm = rmp_obj.data[seg].derivative('mag', 'temp', smoothing=5)
-        norm = ymax
-        lines = ax.plot(segm['temp'].v, segm['mag'].v / norm , '-', color=color[seg],
-                        alpha = 0.2)
+        segm = rmp_obj.data[seg].derivative('mag', 'temp', smoothing=3)
+        lines = ax.plot(segm['temp'].v, -segm['mag'].v,
+                        marker=marker,
+                        ls=ls,
+                        color=color[seg],
+                        alpha=alpha)

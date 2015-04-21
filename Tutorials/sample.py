2e257741643b995f461b0ad30f8dfbc1f09214fa
Mike Volk <grossstadtlegende@gmail.com>
2015-04-21 14:37:53 +0200

    get_sample_with_multiple_hys implemented

--- Tutorials/sample.py
+++ Tutorials/sample.py
@@ -3,7 +3,7 @@ import os
 from os.path import join
 import RockPy
 import itertools
-
+from copy import deepcopy
 
 def get_hys_all_sample():
     folder = RockPy.test_data_path
@@ -49,13 +49,32 @@ def get_pmd_demag():
     S.add_measurement(mtype='afdemag', mfile=dm, machine='pmd')
     return S
 
+def get_sample_with_multiple_hys():
+    sample = RockPy.Sample(name='test_sample',
+                           mass=34.5, mass_unit='mg',
+                           diameter=5.4, height=4.3, length_unit='mm',
+                           treatments='pressure_0.0_GPa; temperature_300.0_C')
+    vftb_hys_file = join(RockPy.test_data_path, 'vftb', 'MUCVFTB_test.hys')
+    m1 = sample.add_measurement(mtype='hys', machine='vftb', mfile=vftb_hys_file,
+                           treatments='pressure_0.0_GPa; temperature_100.0_C')
+
+    m2 = deepcopy(m1)
+    m3 = deepcopy(m2)
+
+    for dtype in m2.data:
+        for column in m2.data[dtype].column_names:
+            m2.data[dtype][column] = 0.1 + m2.data[dtype][column].v
+
+    for dtype in m3.data:
+        for column in m3.data[dtype].column_names:
+            m3.data[dtype][column] = 0.1 + m3.data[dtype][column].v
+    return sample
 
 def get_hys_with_multiple_cond():
     sample = RockPy.Sample(name='test_sample',
                            mass=34.5, mass_unit='mg',
                            diameter=5.4, height=4.3, length_unit='mm',
                            treatments='pressure_0.0_GPa; temperature_300.0_C')
-
 
     # vftb
     vftb_hys_file = join(RockPy.test_data_path, 'vftb', 'MUCVFTB_test.hys')

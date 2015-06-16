71ab3d4a0dfb7bc94cbd42bba7ce21fe412f4352
Mike Volk <grossstadtlegende@gmail.com>
2015-06-16 16:53:02 +0200

    add_series -> add_sval refactor

--- Tutorials/thellier_is_treatment.py
+++ Tutorials/thellier_is_treatment.py
@@ -9,7 +9,7 @@ def test():
     sample = Sample(name='1a')
     M = sample.add_measurement(mtype='thellier', mfile=cryomag_file, machine='cryomag')
     M.set_initial_state(mtype='trm', mfile=cryomag_is_file, machine='cryomag')
-    M.add_series(stype='pressure', sval=0.0)
+    M.add_svalue(stype='pressure', sval=0.0)
     M.series[0].add_value('max_p', 0.6)
     M.series[0].add_value('tonnage', 3)
     M.series[0].add_value('release_time', 30.0)
